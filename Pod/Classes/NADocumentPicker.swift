e2225b59b65014a7cb490ae33f87b1bd2b15609b
Burak Yanmaz <burakyanmaz@buraks-macbook-pro.local>
2017-02-17 16:42:53 +0300

    Document picker mode changed to UIDocumentPickerMode.Import

--- Pod/Classes/NADocumentPicker.swift
+++ Pod/Classes/NADocumentPicker.swift
@@ -12,13 +12,13 @@
 import UIKit
 import MobileCoreServices
 import BrightFutures
-import AnyError
 import NACommonUtils
+import Result
 
 /**
  Error type for `NADocumentPicker`
  
-  - NoDocumentPicked: is the error returned by the `Future`
+ - NoDocumentPicked: is the error returned by the `Future`
  from `show` when no document is choosen by the user.
  */
 public enum NADocumentPickerErrors: ErrorType {
@@ -32,7 +32,7 @@ public enum NADocumentPickerErrors: ErrorType {
 /**
  Encapsulates UIKit document picker UI, providing a simple API.
  `show` is the only API entry.
-*/
+ */
 public class NADocumentPicker : NSObject {
     private let parentViewController: UIViewController
     private var keepInMemory: NADocumentPicker?
@@ -42,13 +42,12 @@ public class NADocumentPicker : NSObject {
      Shows the document picker, returning a `Future` containing the document picked
      or `NoDocumentPicked`.
      
-     See also: 
+     See also:
      - [nickager.com](http://nickager.com/blog/2016/03/07/DocumentPicker)
      - [github](https://github.com/NickAger/NADocumentPicker)
      
      - Parameter view:  The view from which the popover document menu appears
      - Parameter parentViewController: The associated parent view controller
-     - Parameter documentTypes: An array of document types to be opened, by default PlainText
      
      - Returns: A `Future` containing the document picked or `NoDocumentPicked`
      */
@@ -56,39 +55,40 @@ public class NADocumentPicker : NSObject {
         let instance = NADocumentPicker(parentViewController: parentViewController)
         return instance.showDocumentProviderMenu(from: view, parentViewController: parentViewController, documentTypes: documentTypes)
     }
-
+    
     /*private*/ init(parentViewController: UIViewController) {
         self.parentViewController = parentViewController
         super.init()
-
+        
         keepInMemoryUntilComplete()
     }
-
+    
     private func showDocumentProviderMenu(from view: UIView, parentViewController: UIViewController, documentTypes: [String]) -> Future<NSURL, AnyError> {
         let activityOverlay = showActivityOverlayAddedTo(parentViewController.view)
-        let documentProviderMenu = UIDocumentMenuViewController(documentTypes:documentTypes, inMode: UIDocumentPickerMode.Open)
+        
+        let documentProviderMenu = UIDocumentMenuViewController(documentTypes:documentTypes, inMode: UIDocumentPickerMode.Import)
         documentProviderMenu.delegate = self
         if let popoverPresentationController = documentProviderMenu.popoverPresentationController {
             popoverPresentationController.sourceView = view
             popoverPresentationController.sourceRect = view.bounds
         }
-
+        
         parentViewController.presentViewController(documentProviderMenu, animated: true, completion: {
             activityOverlay.removeFromSuperview()
             print("\n</layout-errors>\nSee: http://openradar.appspot.com/19385063\n")
         })
-
+        
         return promise.future
     }
-
+    
     private func keepOurselvesInMemory() {
         keepInMemory = self
     }
-
+    
     private func freeOurselvesFromMemory() {
         keepInMemory = nil
     }
-
+    
     private func keepInMemoryUntilComplete() {
         keepOurselvesInMemory()
         self.promise.future.onComplete { [unowned self] _ in
@@ -106,7 +106,7 @@ extension NADocumentPicker : UIDocumentMenuDelegate {
             activityOverlay.removeFromSuperview()
         })
     }
-
+    
     public func documentMenuWasCancelled(_: UIDocumentMenuViewController) {
         promise.failure(NADocumentPickerErrors.NoDocumentPicked.asAnyError())
     }
@@ -117,7 +117,7 @@ extension NADocumentPicker : UIDocumentPickerDelegate {
     public func documentPicker(_: UIDocumentPickerViewController, didPickDocumentAtURL url: NSURL) {
         promise.success(url)
     }
-
+    
     public func documentPickerWasCancelled(_: UIDocumentPickerViewController) {
         promise.failure(NADocumentPickerErrors.NoDocumentPicked.asAnyError())
     }
